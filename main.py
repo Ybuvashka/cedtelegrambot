5bc0f7802162c9b2b994761e1736b235a8c98b7c
Ybuvashka <ilkivaoleksandra2@gmail.com>
2022-06-10 08:17:21 +0300

    new commit

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def week(message):
             f"join teachers on teachers.teacher_id = teachers_subjects.teacher_id "
             f"join groups_subjects on subjects.subject_id = groups_subjects.subject_id "
             f"join groups on groups.group_id = groups_subjects.group_id "
-            f"where {second_param} = {fk_id} and  subjects.subject_weekday = {weekdays[i]} "
+            f"where {second_param} = {fk_id} and  subjects.subject_weekday = '{weekdays[i]}' "
             f"order by subjects.subject_weekday,subjects.subject_number asc "
         )
         result = db_object.fetchall()
