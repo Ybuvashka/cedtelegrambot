380b2f564576fb9bdae6a91c9b6c6ad2168b20e0
Ybuvashka <ilkivaoleksandra2@gmail.com>
2022-06-13 10:28:35 +0300

    new commit

--- main.py
+++ main.py
@@ -178,11 +178,11 @@ def check_user_fk(message):
         teacher_id = row[1]
         group_id = row[2]
 
-    if "Студент" != user_role:
+    if  user_role != "Студент":
         first_param = f"teachers.teacher_name"
         second_param = f"groups.group_id"
         fk_id = group_id
-    elif "Викладач" != user_role:
+    elif user_role != "Викладач":
         first_param = f"groups.group_name"
         second_param = f"teachers.teacher_id"
         fk_id = teacher_id
