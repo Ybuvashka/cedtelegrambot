8f29a1dc252615d00e5a5c977d1bce5c8ffa7376
Ybuvashka <ilkivaoleksandra2@gmail.com>
2022-06-16 00:41:56 +0300

    new commit

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def today(message, day):
         f"join teachers on teachers.teacher_id = teachers_subjects.teacher_id "
         f"join groups_subjects on subjects.subject_id = groups_subjects.subject_id "
         f"join groups on groups.group_id = groups_subjects.group_id "
-        f"where {second_param} = {fk_id} and subjects.subject_weekday = any ({day}) order by subjects.subject_number asc"
+        f"where {second_param} = {fk_id} and subjects.subject_weekday LIKE '{day}%' order by subjects.subject_number asc"
     )
     result = db_object.fetchall()
 
